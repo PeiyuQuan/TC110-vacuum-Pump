e012fdce0208d9cbe0f99b6eab5efaa797213c4b
Peiyu Quan <41603034+PeiyuQuan@users.noreply.github.com>
2022-03-24 18:50:58 -0700

    Update serial.cmd

--- serial.cmd
+++ serial.cmd
@@ -4,7 +4,9 @@
 #drvAsynSerialPortConfigure("portName","ttyName",priority,noAutoConnect,noProcessEosIn)
 #asynSetOption("portName",addr,"Key","value")
 
-drvAsynSerialPortConfigure("serial1", "COM13", 0, 0, 0)
+drvAsynSerialPortConfigure("serial1", "/dev/ttyUSB0", 0, 0, 0)
+asynOctetSetInputEos("serial1",0,"\r")
+asynOctetSetOutputEos("serial1",0,"\r")
 asynSetOption("serial1",0,"baud","9600")
 asynSetOption("serial1",0,"bits","8")
 asynSetOption("serial1",0,"stop","1")
